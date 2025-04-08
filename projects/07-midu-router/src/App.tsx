f76c09731dbf60175de903bb5e099afd3efef5f8
P4TTT0 <patricio.perezcardenal@gmail.com>
2025-04-07 21:27:00 -0300

    feat: components lazy load

--- projects/07-midu-router/src/App.tsx
+++ projects/07-midu-router/src/App.tsx
@@ -1,9 +1,15 @@
-import { HomePage } from "./components/HomePage";
-import { AboutPage } from "./components/AboutPage";
+import { HomePage } from "./components/HomePage"; // Import estatico
 import { RouteType } from "./types/routeType";
 import { Router } from "./components/Router";
 import { SearchPage } from "./components/SearchPage";
 import { Route } from "./components/Route";
+import { lazy } from "react";
+
+// Cargar SOLO lo que el usuario necesite 
+const LazyAboutPage = lazy(() =>
+  import('./components/AboutPage').then((mod) => ({ default: mod.AboutPage }))
+);
+
 
 const routes: RouteType[] = [
   {
@@ -17,7 +23,7 @@ function App() {
     <main>
       <Router routes={routes}>
         <Route path="/" component={HomePage}></Route>
-        <Route path="/about" component={AboutPage}></Route>
+        <Route path="/about" component={LazyAboutPage}></Route>
       </Router>
     </main>
   )
