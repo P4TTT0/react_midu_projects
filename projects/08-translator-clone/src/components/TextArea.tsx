7889328ca6d56b82a103c8c8a533ba51936e74a2
P4TTT0 <patricio.perezcardenal@gmail.com>
2025-04-16 22:24:58 -0300

    style: added bottoms icons

--- projects/08-translator-clone/src/components/TextArea.tsx
+++ projects/08-translator-clone/src/components/TextArea.tsx
@@ -2,6 +2,7 @@ import React, { ChangeEvent } from "react"
 import { Form } from "react-bootstrap"
 import './TextArea.css'
 import { SectionType } from "../types/translate"
+import { ClipboardIcon, SpeakerIcon } from "./Icons"
 
 type TextAreaProps = 
     | { type: SectionType.From, onChange: (value: string) => void, value: string }
@@ -26,8 +27,15 @@ export const TextArea: React.FC<TextAreaProps> = (props) => {
                 disabled={isTo}
                 onChange={(event: ChangeEvent<HTMLTextAreaElement>) => props.onChange(event.target.value)}
             />
-
-            { !isTo && <span className="caract-len">{props.value.length} / 5000</span> }
+            <section className="bottom-icons">
+                <div>
+                    <button><SpeakerIcon /></button>
+                </div>
+                <div>
+                    { !isTo && <div className="caracters-container"><span>{props.value.length} / 5000</span></div> }
+                    { isTo && <button onClick={() => navigator.clipboard.writeText(props.value)}><ClipboardIcon/></button>}
+                </div>
+            </section>
         </>
     )
 }
