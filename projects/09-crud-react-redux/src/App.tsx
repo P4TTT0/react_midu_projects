c3a774176189d23ac5fb446942a35136d95310e9
P4TTT0 <patricio.perezcardenal@gmail.com>
2025-04-21 23:04:34 -0300

    feat: new redux project

---
+++ projects/09-crud-react-redux/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Table from './components/Table'
+
+function App() {
+  return (
+    <>
+      <h1>Proyecto con Redux</h1>
+      <Table/>
+    </>
+  )
+}
+
+export default App
